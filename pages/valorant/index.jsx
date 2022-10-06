e4910ad5b30acc0089da908a7e8b2049af5df905
miroz devkota <mirozxy@gmail.com>
2022-10-06 21:19:29 +0545

    clean up the design

--- pages/valorant/index.jsx
+++ pages/valorant/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { InView } from 'react-intersection-observer';
 
 import {
   Container,
@@ -11,15 +12,17 @@ import {
   Role_Container,
   Role,
   Biography,
+  Role_Pic,
 } from '../../style/valorant/style';
 
 import { Valorant } from '../../Data/valorant';
-import { useState } from 'react';
 
 import ScrollContainer from 'react-indiana-drag-scroll';
 
 function Index(props) {
   const [activeAgent, setActiveAgent] = useState(Valorant[0]);
+
+  useEffect(() => {}, [activeAgent]);
 
   return (
     <Container>
@@ -28,9 +31,27 @@ function Index(props) {
           <ScrollContainer draggingClassName="container--dragging" className="container">
             {Valorant.map((agent) => {
               return (
-                <Character_Name key={agent.id} onClick={() => setActiveAgent(agent)}>
-                  {agent.name}
-                </Character_Name>
+                <InView
+                  as={`div`}
+                  key={agent.id}
+                  onChange={(inView, ref, entry) =>
+                    console.log('inview', { inView }, { ref }, { entry })
+                  }
+                >
+                  <Character_Name
+                    selected={activeAgent.id == agent.id ? 1 : 0}
+                    key={agent.id}
+                    id={agent.id}
+                    onClick={() => {
+                      setActiveAgent(agent);
+                      const link = document.getElementById(agent.id);
+                      link.scrollIntoView();
+                    }}
+                  >
+                    <span className="agentNo">{agent.id}</span>
+                    <span className="agentName">{agent.name}</span>
+                  </Character_Name>
+                </InView>
               );
             })}
           </ScrollContainer>
@@ -39,14 +60,21 @@ function Index(props) {
           <img src={activeAgent.img} />
         </Character>
         <Character_Info>
-          <Role_Container>
-            <span>// ROLE</span>
-            <Role>{activeAgent.role}</Role>
-          </Role_Container>
-          <Biography_Container>
-            <span>// BIOGRAPHY</span>
-            <Biography>{activeAgent.biography}</Biography>
-          </Biography_Container>
+          <main>
+            <Role_Container>
+              <span className="role">// ROLE</span>
+              <Role>
+                <span>{activeAgent.role}</span>
+                <Role_Pic>
+                  <img src={activeAgent.role_pic} />
+                </Role_Pic>
+              </Role>
+            </Role_Container>
+            <Biography_Container>
+              <span className="bio">// BIOGRAPHY</span>
+              <Biography>{activeAgent.biography}</Biography>
+            </Biography_Container>
+          </main>
         </Character_Info>
       </Main>
     </Container>
--- style/valorant/style.js
+++ style/valorant/style.js
@@ -1,7 +1,13 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const breatheAnimation = keyframes`
+ 0% { height: 100px; width: 100px; opacity: 0.2; }
+
+ 100% { height: 100%; width: 100%; opacity: 1; }
+`;
 
 export const Container = styled.div`
-  background: #0f1823;
+  background: linear-gradient(180deg, #0f1823 70%, #ece8e1 30%);
   width: 100%;
   height: 100vh;
   display: flex;
@@ -13,7 +19,6 @@ export const Main = styled.div`
   width: 80vw;
   height: 80vh;
   margin: auto auto;
-  border: 2px solid white;
   display: flex;
   flex-direction: row;
 `;
@@ -26,14 +31,43 @@ export const Character_List = styled.div`
   font-weight: 800;
   overflow: auto;
   width: 100%;
+  height: 100%;
+  max-height: 75%;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const Character_Name = styled.div`
   color: white;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+
+  .agentNo {
+    font-size: 20px;
+    font-weight: 500;
+    height: 100%;
+    margin-top: 35px;
+    color: ${({ selected }) => (selected == 1 ? '#ff4655' : 'white')};
+  }
+
+  .agentName {
+    padding: 0;
+    color: ${({ selected }) => (selected == 1 ? '#ff4655' : 'white')};
+  }
 `;
 
 export const Character = styled.div`
   width: 100%;
+  min-width: 500px;
+  min-height: 80vh;
+  padding: 30px 0;
+  /* z-index: 1000; */
+  animation: ${breatheAnimation} 0.5s ease-in;
+
   img {
     width: 100%;
     height: 100%;
@@ -44,24 +78,72 @@ export const Character = styled.div`
 
 export const Character_Info = styled.div`
   width: 100%;
+  margin: auto 0;
+  height: 100%;
+  max-height: 75%;
+
+  main {
+    padding: 0 50px;
+  }
+
   span {
     color: white;
     font-weight: 600;
   }
 `;
 
-export const Role_Container = styled.div``;
+export const Role_Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
 
-export const Role = styled.div`
-  font-size: 40px;
-  font-weight: 800;
-  color: white;
-  text-transform: uppercase;
+  .role {
+    margin-bottom: 10px;
+  }
 `;
 
-export const Biography_Container = styled.div``;
+export const Role = styled.div`
+  color: white;
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+
+  span {
+    padding: 0 !important;
+    font-size: 47px;
+    font-weight: 700;
+    letter-spacing: -1px;
+  }
+`;
+
+export const Role_Pic = styled.div`
+  width: 25px;
+  height: 100%;
+  height: 25px;
+  margin-top: -20px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    display: block;
+    object-fit: contain;
+  }
+`;
+
+export const Biography_Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .bio {
+    margin-bottom: 20px;
+  }
+`;
 
 export const Biography = styled.div`
   color: white;
   max-width: 100%;
+  border-bottom: 2px solid white;
+  padding-bottom: 60px;
 `;
